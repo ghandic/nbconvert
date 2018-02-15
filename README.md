acc3ab69a3fc2654b455e997cd5df9befc4823d4
Andy Challis <andy.challis@capgemini.com>
2018-02-15 00:42:59 +0000

    init

---
+++ README.md
@@ -0,0 +1,14 @@
+# How it works
+
+Simply add a shortcode and a url to the notebook file
+
+[nbconvert url="https://github.com/python-visualization/folium/blob/master/examples/CheckZorder.ipynb"]
+
+PHP then sends the url to an nbviewer API that will convert the ipynb file to html
+
+## Next steps
+
+### CSS
+- Some more work may be needed to make the html snippet to display like a an ipynb
+- We may need to make some tests to check that it is working on different themes?
+
---
+++ nbconvert.php
@@ -0,0 +1,57 @@
+<?php
+   /*
+   Plugin Name: NbConvert
+   Description: A plugin to add ipynb files to a blog post or page using nbviewer
+   Version: 1.0
+   Author: Andrew Challis
+   Author URI: http://www.andrewchallis.com
+   License: MIT
+   */
+
+//tell wordpress to register the nbconvert shortcode
+
+add_shortcode("nbconvert", "nbconvert_handler");
+
+function nbconvert_handler($atts) {
+  //run function that actually does the work of the plugin
+  $nb_output = nbconvert_function($atts);
+  //send back text to replace shortcode in post
+  return $nb_output;
+}
+
+function nbconvert_function($atts) {
+  //process plugin
+  extract(shortcode_atts(array(
+        'url' => "",
+     ), $atts));
+
+  $clean_url = preg_replace('#^https?://#', '', rtrim($url,'/'));
+  $html = file_get_contents("https://nbviewer.jupyter.org/url/" . $clean_url);
+  $nb_output = getHTMLByID('notebook-container', $html);
+  //send back text to calling function
+  return $nb_output;
+}
+
+function innerHTML(DOMNode $elm) {
+  $innerHTML = '';
+  $children  = $elm->childNodes;
+
+  foreach($children as $child) {
+    $innerHTML .= $elm->ownerDocument->saveHTML($child);
+  }
+
+  return $innerHTML;
+}
+
+function getHTMLByID($id, $html) {
+    $dom = new DOMDocument;
+    libxml_use_internal_errors(true);
+    $dom->loadHTML($html);
+    $node = $dom->getElementById($id);
+    if ($node) {
+        $inner_output = innerHTML($node);
+        return $inner_output;
+    }
+    return FALSE;
+}
+?>
