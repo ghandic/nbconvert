3505e2670447f308cdd3c8fd9114b2a7b1185e12
Andy Challis <andy.challis@capgemini.com>
2018-02-15 02:44:44 +0000

    db

--- nbconvert.php
+++ nbconvert.php
@@ -50,6 +50,8 @@ function get_most_recent_git_change_for_file($url) {
     $datetimes[] = date_create_from_format('Y-m-d\TH:i:sZ', $datetime);
   }
 
+  print_r(max($datetimes))
+
   $max_date = date('Y-m-d H:i:s', max($datetimes));
   return $max_date;
   
