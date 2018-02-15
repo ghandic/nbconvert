5f28005f619451d8c4883f3e1f02186095148faf
Andy Challis <andy.challis@capgemini.com>
2018-02-15 02:09:59 +0000

    db

--- nbconvert.php
+++ nbconvert.php
@@ -43,10 +43,10 @@ function get_most_recent_git_change_for_file($url) {
   
   $dates = array();
   //Iterate over the extracted links and display their URLs
-  foreach ($timae_agos as $time_ago){
+  foreach ($time_agos as $time_ago){
       //Extract and show the "href" attribute. 
-      $dates[] = $time_ago->getAttribute('datetime'), '<br>';
-  }
+      $time_ago->getAttribute('datetime'), '<br>';
+  };
   print_r($dates);
 
   foreach($html->find('time-ago') as $element) {
