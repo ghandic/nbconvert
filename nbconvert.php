c2585ab6e0d344fca3a40c0e25016f0021002cb7
Andy Challis <andy.challis@capgemini.com>
2018-02-15 02:36:18 +0000

    db

--- nbconvert.php
+++ nbconvert.php
@@ -88,7 +88,7 @@ function nbconvert_function($atts) {
   
   //send back text to calling function
   return '<div class="nbconvert-notebook">
-            <label><a href="'. $url . '" target="_blank">Check it out on github, last updated:' . '</a></label>' . $nb_output . '</div>';
+            <label><a href="'. $url . '" target="_blank">Check it out on github, last updated:' . $last_update_date_time . '</a></label>' . $nb_output . '</div>';
 }
 
 function innerHTML(DOMNode $elm) {
